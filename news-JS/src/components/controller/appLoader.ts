b21b737cdffbcd52cb54b76b37e6ab1ed42f728b
Roman Lysiuk <gekalo1025@gmail.com>
2022-12-06 11:27:51 +0200

    refactor: migration to ts appLoader.js and add proxy  and apiKey

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+        super('https://newsapi-redirect-production.up.railway.ap/', {
+            apiKey: '616e6a593977466c8708fa288220ff38',
         });
     }
 }
